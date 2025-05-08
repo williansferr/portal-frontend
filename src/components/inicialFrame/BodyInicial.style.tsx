c331eee9b925ffdece9a98dd9d0aae847d1828a6
WILLIANS FERREIRA DA SILVA <willians.fsilva@eseed646v1i.SEED>
2025-05-08 09:59:37 -0300

    Tela de Entrada em condições de Uso; Tela estilizada com a cara da SEED

--- src/components/inicialFrame/BodyInicial.style.tsx
+++ src/components/inicialFrame/BodyInicial.style.tsx
@@ -10,139 +10,54 @@ import {
   CardHover,
   ContainerCardHover,
   CardSpan,
+  DivCentralize,
+  LabelCoordenacao,
+  LogParanaSEED,
 } from "./styled";
 import iconGestao from "../../assets/iconGestaoSeguranca.png";
 import iconSuporte from "../../assets/iconSuporteTecnico02.png";
 import iconRobotica from "../../assets/iconRoboticaFundoEscuro.png";
+import LogoSecretariaPR from "../../assets/iconGovEstadualSEED.png";
 
 function BodyInicial() {
-  interface Props {
-    as?: keyof HTMLElementTagNameMap; //A interface map contém cada nome de tag HTML especificado e seu tipo de interface correspondente
-    children: React.ReactNode;
-    className?: string;
-    [key: string]: any; // Permite passar outras props, incluindo eventos
+  function selectElement() {
+    const effect = ContainerCardHover;
+    console.log("Entrou!");
+    console.log(effect);
+    return effect;
   }
 
-  const DynamicTag: React.FC<Props> = ({
-    as: Element = "p",
-    children,
-    className,
-    ...rest
-  }) => {
-    switch (Element) {
-      case "p":
-        return (
-          <p className={className} {...rest}>
-            {children}
-          </p>
-        );
-      case "span":
-        return (
-          <span className={className} {...rest}>
-            {children}
-          </span>
-        );
-      
-      // Adicione mais tags conforme necessário
-      default:
-        return (
-          <ContainerCard className={className} {...rest}>
-            {children}
-          </ContainerCard>
-        );
-    }
-  };
-
-  interface MeuComponenteProps {
-    defaultTag?: any;
-    hoverTag?: any;
-    children: React.ReactNode;
-    className?: string;
-  }
-  // interface MeuComponenteProps {
-  //   defaultTag?: keyof HTMLElementTagNameMap;
-  //   hoverTag?: keyof HTMLElementTagNameMap;
-  //   children: React.ReactNode;
-  //   className?: string;
-  // }
-
-  const MeuComponenteInterativo: React.FC<MeuComponenteProps> = ({
-    defaultTag = ContainerCard,
-    hoverTag = ContainerCardHover,
-    children,
-    className,
-  }) => {
-    const [currentTag, setCurrentTag] = useState(hoverTag);
-    // const [currentTag, setCurrentTag] = useState<keyof HTMLElementTagNameMap>(defaultTag);
-
-    // const [valor, setValor] = useState(false); //test by Will
-
-    const handleMouseEnter = () => {
-      // console.log("entrou");
-      console.log(document.getElementsByTagName("ContainerCardHover"));
-      setCurrentTag(ContainerCardHover);
-    };
-
-    const handleMouseLeave = () => {
-      // console.log("saiu");
-      setCurrentTag(defaultTag);
-    };
-
-    return (
-      <DynamicTag
-        as={currentTag}
-        className={className}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        {children}
-      </DynamicTag>
-    );
-  };
-
-  // const ExemploInterativo: React.FC = () => {
-  //   return (
-  //     <div>
-  //       <MeuComponenteInterativo >
-  //         Passe o mouse sobre este parágrafo para transformá-lo em um título!
-  //       </MeuComponenteInterativo>
-  //       <MeuComponenteInterativo
-  //         defaultTag="span"
-  //         hoverTag="div"
-  //         className="destacado"
-  //       >
-  //         Passe o mouse sobre este span para transformá-lo em uma div com
-  //         estilo.
-  //       </MeuComponenteInterativo>
-  //     </div>
-  //   );
-  // };
+  const estiloInicial = ContainerCard;
+  const [enterHover, setEnterHover] = useState(false);
 
   return (
     <InicialFrame>
-      <LabelTitulo>Selecione a Coordenação:</LabelTitulo>
+      <LogParanaSEED src={LogoSecretariaPR} />
+      <DivCentralize>
+        <LabelTitulo>Selecione a Coordenação:</LabelTitulo>
 
-      <Container>
-        <a href="gestao.html">
-          <MeuComponenteInterativo>
-            <CardHover src={iconGestao} />
-            Gestão de Sistemas
-          </MeuComponenteInterativo>
-        </a>
-        {/* <a href="suporte.html" className={Card}> */}
-        <a href="suporte.html">
-          <MeuComponenteInterativo>
-            <CardHover src={iconSuporte} />
-            Suporte Técnico
-          </MeuComponenteInterativo>
-        </a>
-        <a href="robotica.html">
-          <MeuComponenteInterativo>
-            <CardHover src={iconRobotica} />
-            Adm Robótica
-          </MeuComponenteInterativo>
-        </a>
-      </Container>
+        <Container>
+          <a href="gestao.html">
+            <ContainerCard>
+              <CardHover src={iconGestao} />
+              <LabelCoordenacao>Gestão de Sistemas</LabelCoordenacao>
+            </ContainerCard>
+          </a>
+          {/* <a href="suporte.html" className={Card}> */}
+          <a href="suporte.html">
+            <ContainerCard>
+              <CardHover src={iconSuporte} />
+              <LabelCoordenacao>Suporte Técnico</LabelCoordenacao>
+            </ContainerCard>
+          </a>
+          <a href="robotica.html">
+            <ContainerCard>
+              <CardHover src={iconRobotica} />
+              <LabelCoordenacao>Robótica</LabelCoordenacao>
+            </ContainerCard>
+          </a>
+        </Container>
+      </DivCentralize>
     </InicialFrame>
   );
 }
